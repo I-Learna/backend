9d47ca869f71405ce2a15a9ee3f718045d6e7988
Ahmed Medhat <ahmedmedhat1231@gmail.com>
2025-01-11 15:00:17 +0200

    applying strict validations

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,25 +1,62 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { 
+    name: {
         type: String,
-        enum: ['User', 'Admin', 'OperationTeam', 'PaymentTeam'], 
-        default: 'user' 
-    } 
+        required: [true, 'Name is required'],
+        trim: true,
+        minlength: [3, 'Name must be at least 3 characters long'],
+        maxlength: [50, 'Name cannot exceed 50 characters'],
+        validate: {
+            validator: function (value) {
+                return /^[a-zA-Z\s]+$/.test(value); // only alphabets and spaces
+            },
+            message: 'Name must contain only letters and spaces',
+        },
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: validator.isEmail, 
+            message: 'Invalid email format',
+        },
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [8, 'Password must be at least 8 characters long'],
+        validate: {
+            validator: function (value) {
+                // ensuringg password contains at least one uppercase letter, one lowercase letter, one digit, and one special character
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
+            },
+            message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+        },
+    },
+    role: {
+        type: String,
+        enum: {
+            values: ['User', 'Admin', 'OperationTeam', 'PaymentTeam'],
+            message: '{VALUE} is not a valid role',
+        },
+        default: 'User',
+    },
 });
 
-// hash pass bef saving
-userSchema.pre('save', async function (next) {
+// hash password before saving
+userSchema.pre('save', async function (next) {A
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
-// compare pass
+// compare password
 userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
