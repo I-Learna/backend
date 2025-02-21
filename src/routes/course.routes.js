f67456f1d0452ade172df09c91023e76a0c86489
Ahmed Medhat <ahmedmedhat1231@gmail.com>
2025-02-21 23:32:30 +0200

    Modifying course controller to handle the uploads dynamically

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/course.controller');
 
-router.route('/').post(courseController.createCourse).get(courseController.getAllCourses);
+router
+  .route('/')
+  .get(courseController.getAllCourses)
+  .post(courseController.uploadCourseFiles, courseController.createCourse);
 
 router
   .route('/:id')
   .get(courseController.getCourseById)
-  .put(courseController.updateCourse)
+  .put(courseController.uploadCourseFiles, courseController.updateCourse)
   .delete(courseController.deleteCourse);
 
 module.exports = router;
